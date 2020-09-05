a943537b663ee11aa98b7c81674cda9edb285a7e
Evgeniy_Utkin <eugene.utkin@gmail.com>
2020-09-05 15:53:27 +0500

    lesson40 start

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,44 @@
+class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render action: 'new'
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render action: 'edit'
+    end
+
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'home/index'
+
+  get 'terms' => 'pages#terms'
+  get 'about' => 'pages#about'
+
+  resource :contacts, only: [:new, :create], path_names: { :new => '' }
+  resources :articles
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
