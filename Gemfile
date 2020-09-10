36a69dd181f6134fe5668aa13bc05b8924744a8d
Evgeniy_Utkin <eugene.utkin@gmail.com>
2020-09-10 18:44:57 +0500

    database_cleaner added

--- Gemfile
+++ Gemfile
@@ -37,7 +37,8 @@ group :development, :test do
   gem 'rspec-rails'
   gem 'capybara', '>= 2.15'
   gem 'shoulda-matchers'
-  gem "factory_girl_rails", "~> 4.0"
+  gem 'factory_girl_rails', '~> 4.0'
+  gem 'database_cleaner'
 end
 
 group :development do
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,6 +9,7 @@ require 'rspec/rails'
 require 'support/factory_girl'
 require 'capybara/rspec'
 require 'support/session_helper'
+require 'support/database_cleaner'
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
