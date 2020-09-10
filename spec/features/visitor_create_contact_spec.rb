f924b7ab0423c3ff497287be34a5c872a81e023d
Evgeniy_Utkin <eugene.utkin@gmail.com>
2020-09-10 16:46:46 +0500

    create contact test updated

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -4,6 +4,6 @@ feature "Contact Creation" do
   scenario "allows access to contacts page" do
     visit '/contacts'
 
-    expect(page).to have_content 'Contact us'
+    expect(page).to have_content I18n.t('contacts.contact_us')
   end  
 end
