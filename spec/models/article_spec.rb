bf08875fa9443965a16e96b7e0d95804f910b458
Evgeniy_Utkin <eugene.utkin@gmail.com>
2020-09-10 11:34:23 +0500

    article spec updated with validation test

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,7 +1,13 @@
 require "rails_helper"
 
 describe Article do
-  
-  it { should have_many :comments }
+  describe "validations" do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :text }
+  end
+
+  describe "associations" do
+    it { should have_many :comments }
+  end
 
 end
