568b2c812cd2b90b595944b953e4dde177aac939
Evgeniy_Utkin <eugene.utkin@gmail.com>
2020-09-10 11:24:49 +0500

    comment and article specs added

---
+++ spec/models/article_spec.rb
@@ -0,0 +1,7 @@
+require "rails_helper"
+
+describe Article do
+  
+  it { should have_many :comments }
+
+end
---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,7 @@
+require "rails_helper"
+
+describe Comment do
+  
+  it { should belong_to :article }
+
+end
